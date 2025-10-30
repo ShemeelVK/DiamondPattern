7ff06d2ddfb832d572a6fd39eaafe5db62c366fd
ShemeelVK <shameelvk95@gmail.com>
2025-10-30 12:33:44 +0530

    Add project files.

---
+++ DiamondPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Diamond
+{
+    public static void Main()
+    {
+        int n = 5;
+
+        //upper part
+        for(int i = 0; i < n; i++)
+        {
+           for(int space = i; space < n - 1; space++)
+            {
+                Console.Write(" ");
+            }
+           for(int star = 0; star < (2 * i) + 1; star++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        //lower part
+        for(int i = n - 2; i >= 0; i--)
+        {
+            for (int space = n - 1; space > i; space--)
+            {
+                Console.Write(" ");
+            }
+            for (int star = 0; star < (2 * i) + 1; star++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
